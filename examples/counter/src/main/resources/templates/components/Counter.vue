72ec1f3e9aea47b474cdc97a4b64b6a15b4f1b65
lh <shiyi.code@qq.com>
2026-08-28 16:53:32 +0800

    feat:add-resource-vue-counter-example

---
+++ examples/counter/src/main/resources/templates/components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1>Counter</h1>
+    <p>{{ count }}</p>
+    <input v-model="count" aria-label="count">
+    <button @click="increment">Increment</button>
+    <button @click="toggle">Toggle message</button>
+    <p v-if="visible">Reactive Thymeleaf is live.</p>
+  </main>
+</template>
